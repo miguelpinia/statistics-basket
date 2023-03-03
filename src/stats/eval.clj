fa91a04b9ead094d0a459ffb6d014e488de2c49b
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-03-03 11:09:18 -0600

    Add some examples.

--- src/stats/eval.clj
+++ src/stats/eval.clj
@@ -5,6 +5,7 @@
             [incanter.io :as iio]
             [incanter.stats :as s]
             [incanter.charts :as c]
+            [incanter.distributions :as d]
             [clojure.walk :refer [keywordize-keys]])
   (:import (java.io EOFException)))
 
@@ -62,38 +63,52 @@
          (sort-by first)
          (map second))))
 
-(defn plot-means [data title]
-  (let [mean-cas    (get-mean data :CAS)
-        mean-cas-t    (get-mean data :CAST)
-        mean-fai    (get-mean data :FAIDELAY)
-        mean-rwnc   (get-mean data :RWNC)
-        mean-rwnc-t   (get-mean data :RWNCT)
-        mean-rwsqrt (get-mean data :RWSQRT)
-        procs (range 1 65)]
-    (-> (c/xy-plot procs mean-cas
-                   :title title
-                   :x-label "Processors"
-                   :y-label "Time (nanoseconds)"
-                   :legend true
-                   :series-label "Mean CAS"
-                   :points true)
-        (c/add-lines procs mean-fai
-                     :series-label "Mean FAI"
-                     :points true)
-        (c/add-lines procs mean-cas-t
-                     :series-label "Mean CAS-T"
-                     :points true)
-        (c/add-lines procs mean-rwnc
-                     :series-label "Mean RWNC"
-                     :points true)
-        (c/add-lines procs mean-rwnc-t
-                     :series-label "Mean RWNC-T"
-                     :points true)
-        (c/add-lines procs mean-rwsqrt
-                     :series-label "Mean RW SQRT"
-                     :points true)
-        #_(i/save (str title ".png"))
-        (i/view))))
+;; (def sequence (range 65 128))
+
+(defn filter-by-8 [sequence]
+  (let [multiple-8 #(= (mod % 8) 0)]
+    (->> sequence
+         (map #(vector %1 %2) (range))
+         (filter #(multiple-8 (inc (first %))))
+         (map second))))
+
+;; (filter-by-8 sequence)
+
+(defn plot-means
+  ([data title]
+   (plot-means data title (range 1 65)))
+  ([data title procs]
+   (let [
+         mean-cas    (get-mean data :CAS)
+         mean-cas-t  (get-mean data :CAST)
+         mean-fai    (get-mean data :FAIDELAY)
+         mean-rwnc   (get-mean data :RWNC)
+         mean-rwnc-t (get-mean data :RWNCT)
+         mean-rwsqrt (get-mean data :RWSQRT)]
+     (-> (c/xy-plot procs mean-cas
+                    :title title
+                    :x-label "Processors"
+                    :y-label "Time (nanoseconds)"
+                    :legend true
+                    :series-label "Mean CAS"
+                    :points true)
+         (c/add-lines procs mean-fai
+                      :series-label "Mean FAI"
+                      :points true)
+         (c/add-lines procs mean-cas-t
+                      :series-label "Mean CAS-T"
+                      :points true)
+         (c/add-lines procs mean-rwnc
+                      :series-label "Mean RWNC"
+                      :points true)
+         (c/add-lines procs mean-rwnc-t
+                      :series-label "Mean RWNC-T"
+                      :points true)
+         (c/add-lines procs mean-rwsqrt
+                      :series-label "Mean RW SQRT"
+                      :points true)
+         #_(i/save (str title ".png"))
+         (i/view)))))
 
 
 
@@ -135,35 +150,9 @@
   (plot-means data-5 "Statistics 20'000'000 operations"))
 
 
-;; data-5
-
-
-(let [json (load-json "data/2023-02-24-13:01:21_experiment_time_execution.json")
-      c-names (map #(keyword (str "iter-" %)) (range (i/$ :iterations json)))
-      data (map #(i/$ % json) c-names)]
-  (plot-means data "Statistics 20'000'000 operations"))
-
-;; f-test
-
 (defn mean [xs]
   (/ (apply + xs)
      (count xs)))
-
-#_(let [data-zero-cas   (get (get-data-by-col data-4 :CAS) 0)
-        data-zero-cas-t (get (get-data-by-col data-4 :CAST) 0)
-        ;; m (mean data-zero)
-        ;; v (variance data-zero)
-        ;; sd (standard-deviation data-zero)
-        ;; ci (confidence-interval 0.05 data-zero)
-        ;; psd (pooled-standard-deviation data-zero-cas data-zero-cas-t)
-        ;; pse (pooled-standard-error data-zero-cas data-zero-cas-t)
-        tstat (t-stat data-zero-cas data-zero-cas-t)
-        ;; df (+ (count data-zero-cas) (count data-zero-cas-t) -2)
-        ttest (t-test data-zero-cas data-zero-cas-t)
-        ]
-    (println tstat ", " ttest))
-
-
 
 ;; (mean (get (get-data-by-col data-3 :CAS) 0))
 
@@ -219,7 +208,7 @@
 
 (defn t-test [a b]
   (let [df (+ (count a) (count b) -2)]
-    (- 1 (s/cdf-t (i/abs (t-stat a b)) :df df))))
+    - (1 s/cdf-t ((i/abs (t-stat a b)) :df df))))
 
 (defn t-test-2-tails [a b]
   (let [df (+ (count a) (count b) -2)]
@@ -242,6 +231,8 @@
         msw (/ (ssw groups) df2)]
     (/ msb msw)))
 
+;; (s/cdf-f 3456 :df1 )
+
 (defn f-test [groups]
   (let [n (count (apply concat groups))
         m (count groups)
@@ -250,35 +241,122 @@
         f-stat (f-stat groups df1 df2)]
     (s/cdf-f f-stat :df1 df1 :df2 df2 :lower-tail? false)))
 
+(def last-file "data/2023-03-01-22:29:34_experiment_time_execution.json")
+(def last-file-40 "data/2023-03-02-00:42:46_experiment_time_execution.json")
+(def last-file-40-wm "data/2023-03-02-09:07:52_experiment_time_execution.json")
+
 (defn group-data [data proc-num]
-  (let [normalize (fn [xs] (map #(/ % 1000000) xs))])
-  (map (comp normalize
+  (let [normalize (fn [xs] (map #(/ % 1000000) xs))]
+    (map (comp normalize
            #(get % proc-num)
            first
            vals)
-     data))
+     data)))
 
-(defn f-test-proc [ds proc]
-  (let [ks (keys (first ds))
-        data (for [k ks]
-             (identity {k (get-data-by-col ds k)}))
-        grouped (group-data data proc)
-        n (count (apply concat grouped))]
-  #_(f-stat grouped (count ap))
-  n))
+;; (defn f-test-proc [ds proc]
+;;   (let [ks      (keys (first ds))
+;;         data    (for [k ks]
+;;                   (identity {k (get-data-by-col ds k)}))
+;;         grouped (group-data data proc)
+;;         n       (count (apply concat grouped))]
+;;   #_(f-stat grouped (count ap))
+;;   n))
 
-(let [ks (keys (first data-4))
-      data (for [k ks]
-             (identity {k (get-data-by-col data-4 k)}))
-      grouped (group-data data 2)
-      n (count (apply concat grouped))]
-  (f-test grouped))
+(defn get-f-test [file proc-num]
+  (let [json      (load-json file)
+        c-names   (map #(keyword (str "iter-" %)) (range (i/$ :iterations json)))
+        data      (map #(i/$ % json) c-names)
+        get-by    (fn [coll kw idx] (->> coll (map kw) (map #(get % idx))))
+        processed (->> data
+                       (first)
+                       (keys)
+                       (map #(identity [% (get-by data % proc-num)]))
+                       (into {})
+                       (vals))
+        n         (count (apply concat processed))
+        m         (count processed)
+        df1       (- m 1)
+        df2       (- n m)]
+    {:f-test (format "%.32f" (f-test processed))
+     :df1 df1
+     :df2 df2}))
 
-;; (f-test-proc data-4 0)
+
+(defn get-t-test [file proc-num & {:keys [group-1 group-2]}]
+  (let [json      (load-json file)
+        c-names   (map #(keyword (str "iter-" %)) (range (i/$ :iterations json)))
+        data      (map #(i/$ % json) c-names)
+        get-by    (fn [coll kw idx] (->> coll (map kw) (map #(get % idx))))
+        processed (->> data
+                       (first)
+                       (keys)
+                       (map #(identity [% (get-by data % proc-num)]))
+                       (into {}))
+        g-1       (group-1 processed)
+        g-2       (group-2 processed)]
+  (s/t-test g-1 :y g-2)))
+
+
+;; Dibujamos la gráfica de los datos
+(let [file    last-file-40-wm
+      json    (load-json file)
+      c-names (map #(keyword (str "iter-" %)) (range (i/$ :iterations json)))
+      data    (map #(i/$ % json) c-names)]
+  (plot-means data "Statistics 10'000'000 operations"))
+
+;; Calcula f-test sobre todos los grupos indicando la cantidad de
+;; procesadores utilizada para la prueba.
+#_(let [file     last-file-40-wm
+      proc-num 50]
+  (get-f-test last-file proc-num))
+
+
+;; Calcula la t-test sobre dos grupos, indicando la cantida de
+;; procesadores utilizados para la prueba. En este ejemplo, queremos
+;; comprobar si, para la versión CAS y la version RWNC hay una
+;; diferencia entre ambos. La hipótesis nula es que ambos procesos son
+;; estadísticamente iguales. La hipótesis alternativa es que hay
+;; diferencias estadisticamente significativas. Dado el p-value y el
+;; t-score para este ejemplo, podemos rechazar nuestra hipotesis nula.
+#_(let [file last-file-40-wm
+      proc-num 51
+      group-1 :FAIDELAY
+      group-2 :RWSQRT]
+  (get-t-test file proc-num :group-1 group-1 :group-2 group-2))
 
 
 
-(let [ks (keys (first data-4))
+#_(let [json      (load-json last-file-40-wm)
+      c-names   (map #(keyword (str "iter-" %)) (range (i/$ :iterations json)))
+      processed (map #(->> json (i/$ %)) c-names)
+      ks        (keys (first processed))
+      _         (print ks)
+      data      (for [k ks]
+                  {k (get-data-by-col processed k)})
+      grouped   (group-data data 51)
+      box-plot  (c/box-plot (first grouped)
+                            :x-label "Operation type"
+                            :y-label "Time (s)")
+      add-box   (fn [chart times]
+                  (c/add-box-plot chart times))]
+    (-> (reduce add-box box-plot (rest grouped))
+        (i/view)))
+
+
+#_(let [json     (load-json "data/2023-02-24-13:01:21_experiment_time_execution.json")
+      ks       (keys (first json))
+      data     (for [k ks]
+                 (identity {k (get-data-by-col json k)}))
+      grouped  (group-data data 0)
+      box-plot (c/box-plot (first grouped)
+                           :x-label "Operation type"
+                           :y-label "Time (ns)")
+      add-box  (fn [chart times]
+                 (c/add-box-plot chart times))]
+  (-> (reduce add-box box-plot (rest grouped))
+      (i/view)))
+
+#_(let [ks (keys (first data-4))
       data (for [k ks]
              (identity {k (get-data-by-col data-4 k)}))
       grouped (group-data data)
@@ -288,5 +366,28 @@
       add-box (fn [chart times]
                 (c/add-box-plot chart times))]
   (-> (reduce add-box box-plot (rest grouped))
-      (i/view))
-  )
+      (i/view)))
+
+
+#_(let [data-zero-cas   (get (get-data-by-col data-4 :CAS) 0)
+        data-zero-cas-t (get (get-data-by-col data-4 :CAST) 0)
+        ;; m (mean data-zero)
+        ;; v (variance data-zero)
+        ;; sd (standard-deviation data-zero)
+        ;; ci (confidence-interval 0.05 data-zero)
+        ;; psd (pooled-standard-deviation data-zero-cas data-zero-cas-t)
+        ;; pse (pooled-standard-error data-zero-cas data-zero-cas-t)
+        tstat (t-stat data-zero-cas data-zero-cas-t)
+        ;; df (+ (count data-zero-cas) (count data-zero-cas-t) -2)
+        ttest (t-test data-zero-cas data-zero-cas-t)
+        ]
+    (println tstat ", " ttest))
+
+#_(let [ks (keys (first data-4))
+        data (for [k ks]
+               (identity {k (get-data-by-col data-4 k)}))
+        grouped (group-data data 2)
+        n (count (apply concat grouped))]
+    (f-test grouped))
+
+;; (f-test-proc data-4 0)
