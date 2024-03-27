d71b30e333ec960e56ec18382ad4dfe9558fdb10
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-03-27 17:39:20 -0600

    Added cheshire package to deal with streams in json files.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license        {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
                    :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies   [[org.clojure/clojure "1.11.1"]
-                   [incanter "1.9.3"]]
+                   [incanter "1.9.3"]
+                   [cheshire "5.12.0"]]
   :resource-paths ["data"]
   :main           ^:skip-aot stats.core
   :target-path    "target/%s"
